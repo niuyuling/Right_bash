d994377d43d876ab56d9ee5f76bb2a6d059a889b
aixiao <aixiao@aixiao.me>
2020-05-23 13:08:12 +0800

    添加开机SSH启动

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,33 @@
-#include <windows.h>
-#include <stdio.h>
-#include <direct.h>
-#include "getopt.h"
+#include "main.h"
+
+// 打印帮助信息
+void Help(char** argv) {
+	static const char* help_s[] = {
+		"",
+		"Options:",
+		"    -h,?    print help",
+		"    -a      add Registry",
+		"    -d      delete Registry",
+		"    -b      开机启动 SSH (默认Debian)",
+		"    -p      关闭开机启动 SSH (默认Debian)",
+		"",
+		0
+	};
+	static const char name[] = "Right_bash 1.1";
+	static const char usage[] =
+		"Usage: [-?had]";
+	static const char author[] = "Author: aixiao@aixiao.me";
+
+	printf("%s\n", name);
+	printf("%s\n", author);
+	printf("%s\n", usage);
+
+	for (int i = 0; help_s[i]; i++)
+		fprintf(stderr, "%s\n", help_s[i]);
+}
 
 // 根据错误码输出错误信息 
-void showErrorText(DWORD error_num)
-{
+void showErrorText(DWORD error_num) {
 	char* msg = NULL;
 	FormatMessage(
 		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
@@ -24,16 +46,14 @@ void showErrorText(DWORD error_num)
 		printf("%sn", msg);
 }
 
-void add()
-{
+void add_Right() {
 	HKEY hKey;
 	HKEY subKey;
 	DWORD result;
 
 	char item1[] = "bash";
-	char item2[] = "command";
-
 	char value1[] = "C:\\Windows\\System32\\bash.exe";
+	char item2[] = "command";
 	char value2[] = "Open Bash in this location";
 
 	// 打开注册表, HKEY_CLASSES_ROOT\Directory\Background\shell
@@ -44,11 +64,21 @@ void add()
 		&hKey                               // 打开之后的句柄
 	);
 
+	if (result != ERROR_SUCCESS) {
+		printf("注册表设置失败\n");
+		showErrorText(result);
+	}
+
 	// 添加注册表项, 即 HKEY_CLASSES_ROOT\Directory\Background\shell\bash
 	RegCreateKey(hKey, item1, &subKey);
 
 	// 修改注册表项，默认的数据
 	result = RegSetValueA(subKey, NULL, REG_SZ, value2, (unsigned long)strlen(value2));
+
+	if (result != ERROR_SUCCESS) {
+		printf("注册表设置失败\n");
+		showErrorText(result);
+	}
 
 	// 打开注册表, HKEY_CLASSES_ROOT\Directory\Background\shell\bash
 	result = RegOpenKeyEx(
@@ -58,17 +88,27 @@ void add()
 		&hKey                               // 打开之后的句柄
 	);
 
+	if (result != ERROR_SUCCESS) {
+		printf("注册表设置失败\n");
+		showErrorText(result);
+	}
+
 	// 添加注册表项, 即 HKEY_CLASSES_ROOT\Directory\Background\shell\bash\command
 	RegCreateKey(hKey, item2, &subKey);
 
 	// 修改默认项的数据
 	result = RegSetValueA(subKey, NULL, REG_SZ, value1, (unsigned long)strlen(value1));
 
-	RegCloseKey(hKey);                      //关闭注册表
-	RegCloseKey(subKey);                    //关闭注册表
+	if (result != ERROR_SUCCESS) {
+		printf("注册表设置失败\n");
+		showErrorText(result);
+	}
+
+	RegCloseKey(hKey);                      // 关闭注册表
+	RegCloseKey(subKey);                    // 关闭注册表
 }
 
-void del()
+void del_Right()
 {
 	HKEY hKey;
 	DWORD result;
@@ -81,47 +121,105 @@ void del()
 		&hKey                               // 打开之后的句柄
 	);
 
+	if (result != ERROR_SUCCESS) {
+		printf("注册表打开失败\n");
+		showErrorText(result);
+	}
+
 	// 删除 HKEY_CLASSES_ROOT\Directory\Background\shell\bash
 	result = RegDeleteTreeA(hKey, "bash");
+
+	if (result != ERROR_SUCCESS) {
+		printf("注册表设置失败\n");
+		showErrorText(result);
+	}
 
 	RegCloseKey(hKey);                      //关闭注册表
 }
 
-void Help(char** argv) {
-	static const char* help_s[] = {
-		"",
-		"Options:",
-		"    -h,?    print help",
-		"    -a      add Registry",
-		"    -d      delete Registry",
-		"",
-		0
-	};
-	static const char name[] = "Right_bash";
-	static const char usage[] =
-		"Usage: [-?had]";
-	static const char author[] = "Author: aixiao@aixiao.me";
+void add_boot_ssh() {
+	HKEY hKey;
+	DWORD result;
 
-	printf("%s\n", name);
-	printf("%s\n", author);
-	printf("%s\n", usage);
+	char item[] = "ssh";
+	char value[] = "C:\\WINDOWS\\system32\\wsl.exe -d debian -u root /usr/sbin/service ssh start";
 
-	for (int i = 0; help_s[i]; i++)
-		fprintf(stderr, "%s\n", help_s[i]);
+	// 计算机\HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run
+	result = RegOpenKeyEx(
+		HKEY_CURRENT_USER, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run",
+		0,                                  // 保留参数必须填 0
+		KEY_WRITE,                          // 打开权限，写入
+		&hKey                               // 打开之后的句柄
+	);
 
+	if (result != ERROR_SUCCESS) {
+		printf("注册表打开失败\n");
+		showErrorText(result);
+	}
 
+	result = RegSetValueEx(
+		hKey,
+		item,							// 键名
+		0,								// 保留参数必须填 0
+		REG_SZ,							// 键值类型为字符串
+		(const unsigned char*)value,	// 字符串首地址
+		sizeof(value)					// 字符串长度
+	);
+
+	if (result != ERROR_SUCCESS) {
+		printf("注册表设置失败\n");
+		showErrorText(result);
+	}
+
+	RegCloseKey(hKey);                      // 关闭注册表
+}
+void del_boot_ssh() {
+	HKEY hKey;
+	DWORD result;
+
+	char item[] = "ssh";
+	char value[] = "C:\\WINDOWS\\system32\\wsl.exe -d debian -u root /usr/sbin/service ssh status";
+
+	// 计算机\HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run
+	result = RegOpenKeyEx(
+		HKEY_CURRENT_USER, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run",
+		0,                                  // 保留参数必须填 0
+		KEY_WRITE,                          // 打开权限，写入
+		&hKey                               // 打开之后的句柄
+	);
+
+	if (result != ERROR_SUCCESS) {
+		printf("注册表打开失败\n");
+		showErrorText(result);
+	}
+
+	// 删除 计算机\HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run 下的 ssh
+	result = RegDeleteValue(hKey, item);
+
+	if (result != ERROR_SUCCESS) {
+		printf("注册表设置失败\n");
+		showErrorText(result);
+	}
+
+	RegCloseKey(hKey);                      // 关闭注册表
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int c;
-	while ((c = getopt(argc, argv, "adh?")) != -1) {
+	while ((c = getopt(argc, argv, "adbph?")) != -1) {
 		switch (c) {
 		case 'a':
-			add();
+			add_Right();
 			break;
 		case 'd':
-			del();
+			del_Right();
+			break;
+		case 'b':
+			add_boot_ssh();
+			break;
+		case 'p':
+			del_boot_ssh();
 			break;
 		case 'h':
 			Help(argv);
---
+++ src/main.h
@@ -0,0 +1,11 @@
+#include <windows.h>
+#include <stdio.h>
+#include <direct.h>
+#include "getopt.h"
+
+void showErrorText(DWORD error_num);
+void add_Right();
+void del_Right();
+void add_boot_ssh();
+void del_boot_ssh();
+void Help(char** argv);
